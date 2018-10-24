6c9d1c01a6ed95735aa70e8ae4a0599e6734b0a3
Farook Ibrahim <farook@transvelo.com>
2018-10-24 18:11:46 +0530

    WC 3.5 comp info added

--- mas-woocommerce-variation-swatches.php
+++ mas-woocommerce-variation-swatches.php
@@ -8,6 +8,7 @@
  * Author URI: https://madrasthemes.com/
  * Requires at least: 4.8
  * Tested up to: 4.8
+ * WC tested up to: 3.5.0
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: mas-wcvs
