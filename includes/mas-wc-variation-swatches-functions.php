689f634a8edca1a10d8f1a64244005f215c31f21
Farook Ibrahim <farook@transvelo.com>
2020-04-03 17:27:39 +0530

    function_exists added

--- includes/mas-wc-variation-swatches-functions.php
+++ includes/mas-wc-variation-swatches-functions.php
@@ -1,71 +1,76 @@
 <?php
-/**
- * Get attribute taxonomies.
- *
- * @return array of objects
- */
-function mas_wcvs_get_swatch_attribute_taxonomies() {
-	// if ( false === ( $attribute_taxonomies = get_transient( 'wc_swatch_attribute_taxonomies' ) ) ) {
+
+if( ! function_exists( 'mas_wcvs_get_swatch_attribute_taxonomies' ) ) {
+	/**
+	 * Get attribute taxonomies.
+	 *
+	 * @return array of objects
+	 */
+	function mas_wcvs_get_swatch_attribute_taxonomies() {
 		global $wpdb;
 
 		$attribute_taxonomies = $wpdb->get_results( "SELECT * FROM " . $wpdb->prefix . "woocommerce_attribute_taxonomies WHERE attribute_type = 'color' OR attribute_type = 'image' OR attribute_type = 'label' order by attribute_name ASC;" );
 
-		// set_transient( 'wc_swatch_attribute_taxonomies', $attribute_taxonomies );
-	// }
-
-	return (array) array_filter( apply_filters( 'woocommerce_swatch_attribute_taxonomies', $attribute_taxonomies ) );
+		return (array) array_filter( apply_filters( 'woocommerce_swatch_attribute_taxonomies', $attribute_taxonomies ) );
+	}
 }
 
-/**
- * Get swatch attribute types
- * 
- * @return array of attribute types
- */
-function mas_wcvs_get_attribute_types() {
-	return mas_wcvs()->attribute_types;
+if( ! function_exists( 'mas_wcvs_get_attribute_types' ) ) {
+	/**
+	 * Get swatch attribute types
+	 * 
+	 * @return array of attribute types
+	 */
+	function mas_wcvs_get_attribute_types() {
+		return mas_wcvs()->attribute_types;
+	}
 }
 
-/**
- * Build WHERE query to get swatch attribute types
- *
- * @return string
- */
-function mas_wcvs_build_swatch_attribute_where_string() {
-	$attribute_types = mas_wcvs_get_attribute_types();
+if( ! function_exists( 'mas_wcvs_build_swatch_attribute_where_string' ) ) {
+	/**
+	 * Build WHERE query to get swatch attribute types
+	 *
+	 * @return string
+	 */
+	function mas_wcvs_build_swatch_attribute_where_string() {
+		$attribute_types = mas_wcvs_get_attribute_types();
 
-	$where = array();
+		$where = array();
 
-	foreach( $attribute_types as $attribute_type ) {
-		$where[] = ' attribute_type = \'' . $attribute_type .'\'';
+		foreach( $attribute_types as $attribute_type ) {
+			$where[] = ' attribute_type = \'' . $attribute_type .'\'';
+		}
+
+		if ( ! empty( $where ) ) {
+			$where_string = 'WHERE ' . implode( ' OR ', $where );
+		}
+
+		echo $where_string;
+
+		return $where_string;
 	}
-
-	if ( ! empty( $where ) ) {
-		$where_string = 'WHERE ' . implode( ' OR ', $where );
-	}
-
-	echo $where_string;
-
-	return $where_string;
 }
 
-/**
- * Get a product attributes type setting.
- *
- * @param mixed $name
- * @return string
- */
-function mas_wcvs_attribute_type( $name ) {
-	global $wc_product_attributes, $wpdb;
+if( ! function_exists( 'mas_wcvs_attribute_type' ) ) {
+	/**
+	 * Get a product attributes type setting.
+	 *
+	 * @param mixed $name
+	 * @return string
+	 */
+	function mas_wcvs_attribute_type( $name ) {
+		global $wc_product_attributes, $wpdb;
 
-	$name = str_replace( 'pa_', '', sanitize_title( $name ) );
+		$name = str_replace( 'pa_', '', sanitize_title( $name ) );
 
-	if ( isset( $wc_product_attributes[ 'pa_' . $name ] ) ) {
-		$type = $wc_product_attributes[ 'pa_' . $name ]->attribute_type;
-	} else {
-		$type = $wpdb->get_var( $wpdb->prepare( "SELECT attribute_type FROM " . $wpdb->prefix . "woocommerce_attribute_taxonomies WHERE attribute_name = %s;", $name ) );
+		if ( isset( $wc_product_attributes[ 'pa_' . $name ] ) ) {
+			$type = $wc_product_attributes[ 'pa_' . $name ]->attribute_type;
+		} else {
+			$type = $wpdb->get_var( $wpdb->prepare( "SELECT attribute_type FROM " . $wpdb->prefix . "woocommerce_attribute_taxonomies WHERE attribute_name = %s;", $name ) );
+		}
+
+		return apply_filters( 'mas_wcvs_attribute_type', $type, $name );
 	}
-
-	return apply_filters( 'mas_wcvs_attribute_type', $type, $name );
 }
 
 if( ! function_exists( 'mas_wcvs_loop_variation' ) ) {
@@ -82,4 +87,5 @@ if( ! function_exists( 'mas_wcvs_loop_variation' ) ) {
 		}
 	}
 }
+
 add_action( 'woocommerce_after_shop_loop_item', 'mas_wcvs_loop_variation', 6 );
